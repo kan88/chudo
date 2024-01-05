b8123b3133e2947dd46d941f63ef1934244fc00d
Евгений Кан <evgenijkan@Evgenijs-MacBook-Air.local>
2024-01-05 22:28:48 +0300

    wife fix

--- src/components/team/Team.jsx
+++ src/components/team/Team.jsx
@@ -13,15 +13,15 @@ const responsive = {
   },
   desktop: {
     breakpoint: { max: 3000, min: 1024 },
-    items: 3,
+    items: 5,
   },
   tablet: {
     breakpoint: { max: 1024, min: 464 },
-    items: 2,
+    items: 3,
   },
   mobile: {
     breakpoint: { max: 464, min: 0 },
-    items: 1,
+    items: 2,
   },
 };
 
@@ -31,6 +31,7 @@ export default function Team({
   return (
     <div className={styles.team}>
       <h2 className={styles.team__title}>{title}</h2>
+      <p className={styles.team__filial}>{filial}</p>
       <div className={styles.team__wrapper}>
         <img className={styles.team__photo} src={photo} alt="" width={300} height={300} />
         <div className={styles.team__info}>
@@ -39,7 +40,6 @@ export default function Team({
         </div>
       </div>
       <p className={styles.team__description}>{description}</p>
-      <p className={styles.team__filial}>{filial}</p>
       <ul className={styles.team__list}>
         <Carousel
           responsive={responsive}
@@ -54,7 +54,7 @@ export default function Team({
         >
           {students.map((student) => (
             <div key={student.name} className={styles.team__item}>
-              <img className={styles.team__image} src={student.image} loading="lazy" alt="фотография участника" />
+              <img className={styles.team__image} src={student.image} alt="фотография участника" />
               <span className={styles.team__name}>
                 {student.name}
               </span>
