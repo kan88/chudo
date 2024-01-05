87d67670d7a8fdb7be19444c8f092f407cdaf2d9
Евгений Кан <evgenijkan@Evgenijs-MacBook-Air.local>
2024-01-05 22:38:52 +0300

    wife fix 2

--- src/components/team/Team.jsx
+++ src/components/team/Team.jsx
@@ -17,11 +17,11 @@ const responsive = {
   },
   tablet: {
     breakpoint: { max: 1024, min: 464 },
-    items: 3,
+    items: 4,
   },
   mobile: {
     breakpoint: { max: 464, min: 0 },
-    items: 2,
+    items: 3,
   },
 };
 
